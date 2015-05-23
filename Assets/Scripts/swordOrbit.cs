b84add11466d3cdd3fc25571493d44f590fba17b
GunborN <adamryanmitchell15@gmail.com>
2015-05-23 00:32:29 -0500

    good sword animation

--- Assets/Scripts/swordOrbit.cs
+++ Assets/Scripts/swordOrbit.cs
@@ -7,7 +7,6 @@ public class swordOrbit : MonoBehaviour {
 	private GameObject sword;
 	private Quaternion swordStep1;
 
-	private Animator[] animators;
 	private Animator[] animators2;
 	private Animator swordAnimator;
 	private Animator gundam_sword;
@@ -15,18 +14,7 @@ public class swordOrbit : MonoBehaviour {
 
 	void Start () 
 	{
-		animators = GetComponents<Animator>();
 		animators2 = GetComponentsInChildren<Animator> ();
-//		foreach(Animator thisAnim  in animators)
-//		{
-//			if(thisAnim.name == "swordOrbit")
-//			{
-//				swordAnimator = thisAnim;
-//			}else if(thisAnim.name == "gundam_sword")
-//			{
-//				gundam_sword = thisAnim;
-//			}
-//		}
 		foreach(Animator thisAnim2 in animators2)
 		{
 			Debug.Log (thisAnim2.name);
@@ -62,15 +50,12 @@ public class swordOrbit : MonoBehaviour {
 	IEnumerator SwingSword()
 	{   
 		swordAnimator.SetBool ("Swing", true);
-		swordAnimator.SetBool ("SwingAround", true);
-
 		gundam_sword.SetBool ("Swing", true);
 
-		yield return new WaitForSeconds(.5f);
+
+		yield return new WaitForSeconds(1f);
 
 		swordAnimator.SetBool ("Swing", false);
-		swordAnimator.SetBool ("SwingAround", false);
-
 		gundam_sword.SetBool ("Swing", false);
 
 	}
